71b1ef36017b7a47d3648f76f34a0abcdd380e1b
FelipeBgc <silvabatistaf@yahoo.com>
2025-12-16 22:35:33 -0300

    ajuste produtos

--- java.js
+++ java.js
@@ -1,13 +1,16 @@
-$(document).ready(function () {
-  $(".productslider").slick({
-    infinite: true,
-    autoSlidesToShow: true,
-    variableWidth: true,
-    swipeToSlide: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    appendArrows: $(".productsliderarrow"),
-    prevArrow: '<span class="material-icons arrow left">navigate_before</span>',
-    nextArrow: '<span class="material-icons arrow right">navigate_next</span>'
-  });
+let cartCount = 0;
+const addToCartButton = document.querySelector(".add-to-cart");
+const cartCountElement = document.getElementById("cart-count");
+
+addToCartButton.addEventListener("click", () => {
+  cartCount++;
+  cartCountElement.textContent = cartCount;
+
+  addToCartButton.textContent = "Added to Cart";
+  addToCartButton.style.backgroundColor = "#45a049";
+
+  setTimeout(() => {
+    addToCartButton.textContent = "Add to Cart";
+    addToCartButton.style.backgroundColor = "#4CAF50";
+  }, 2000);
 });
