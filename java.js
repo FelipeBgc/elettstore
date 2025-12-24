6a6c3b5e231d1d8626075d069f81958616cf0ef1
FelipeBgc <silvabatistaf@yahoo.com>
2025-12-24 18:54:45 -0300

    a

--- java.js
+++ java.js
@@ -61,3 +61,47 @@ function nextImage(){
 
     document.getElementById("radio"+count).checked = true;
 }
+
+
+const searchInput = document.getElementById("search");
+
+searchInput.addEventListener('input', (event) => {
+    const value = formatString(event.target.value);
+
+    const items = document.querySelectorAll('.container-li .item');
+    const noResults = document.getElementById('#no_results');
+
+    let hasResults = false
+
+    if(value != '') {
+    items.forEach(item => {
+        const itemTitle = item.querySelector('.titulo-produto-li').textContent
+        if(formatString(itemTitle).indexOf(value) !== -1) {
+            item.style.display = 'flex';
+            
+            hasResults = true
+        }  else {
+            item.style.display = 'none';
+        }
+    })
+
+    if (hasResults) {
+        noResults.style.display = 'none';
+    }   else {
+        noResults.style.display = 'block';
+    }
+    
+        }  else{
+            items.forEach(item => item.style.display = 'flex');
+
+            noResults.style.display = 'none';
+        }
+});
+
+function formatString(value) {
+    return value
+    .toLowerCase()
+    .trim()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g,'');
+}
