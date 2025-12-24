9d7b98c314bb03e2079eb45f13a299b8028f3074
FelipeBgc <silvabatistaf@yahoo.com>
2025-12-23 23:11:41 -0300

    ajuste

--- java.js
+++ java.js
@@ -45,3 +45,20 @@ for (cart_btn of cart_btns) {
         }, 1000);
     }
 }
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage()
+}, 5000)
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+
+}
