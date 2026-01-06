0104cbdd7bb9ab3be35cfc9dd47189e038e72760
FelipeBgc <silvabatistaf@yahoo.com>
2026-01-06 16:39:15 -0300

    carrinho de compras

---
+++ JavaScript/carrinho.js
@@ -0,0 +1,12 @@
+let iconCart = document.querySelector('.cartIcon');
+let close = document.querySelector('.close')
+let body = document.querySelector('body');
+
+
+iconCart.addEventListener('click', () => {
+    body.classList.toggle('showCart')
+})
+
+close.addEventListener('click', () => {
+    body.classList.toggle('showCart')
+})
