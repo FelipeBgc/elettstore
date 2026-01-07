63362e399a8b5ec33b97c5d9cb51b735aa68c2a0
FelipeBgc <silvabatistaf@yahoo.com>
2026-01-07 20:18:02 -0300

    add carrinho de compras

--- JavaScript/carrinho.js
+++ JavaScript/carrinho.js
@@ -1,12 +1,147 @@
-let iconCart = document.querySelector('.cartIcon');
-let close = document.querySelector('.close')
+let iconCart = document.querySelector('.icon-cart');
+let closeCart = document.querySelector('.close')
 let body = document.querySelector('body');
+let listProductHTML = document.querySelector('.listProduct');
+let listCartHTML = document.querySelector('.listCart');
+let iconCartSpan = document.querySelector('.icon-cart span');
 
+let listProducts = [];
+let carts = [];
 
 iconCart.addEventListener('click', () => {
     body.classList.toggle('showCart')
 })
-
-close.addEventListener('click', () => {
+closeCart.addEventListener('click', () => {
     body.classList.toggle('showCart')
 })
+
+const addDataToHTML = () => {
+    listProductHTML.innerHTML = '';
+    if(listProducts.length > 0){
+        listProducts.forEach(product => {
+            let newProduct = document.createElement('div');
+            newProduct.classList.add('item_c');
+            newProduct.dataset.id = product.id; 
+            newProduct.innerHTML = `
+            <img src="${product.image}" alt="">
+                <h2>${product.name}</h2>
+                <div class="price">R$ ${product.price}</div>
+                <button class="addCart">
+                    Comprar
+                </button>
+                <span class="porc_desconto">-10% OFF</span>
+            `;
+            listProductHTML.appendChild(newProduct);
+        })
+    }
+}
+listProductHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if(positionClick.classList.contains('addCart')){
+        let product_id = positionClick.parentElement.dataset.id;
+        addToCart(product_id);
+    }
+})
+
+const addToCart = (product_id) => {
+    let positionThisProductInCart = carts.findIndex((value) => value.product_id == product_id);
+    if(carts.length <= 0) {
+        carts = [{
+            product_id: product_id,
+            quantity: 1
+        }]
+    }else if(positionThisProductInCart < 0 ){
+        carts.push({
+            product_id: product_id,
+            quantity: 1
+        })
+    }else{
+        carts[positionThisProductInCart].quantity = carts[positionThisProductInCart].quantity + 1;
+    }
+    addCartToHTML();
+    addCartToMemory();
+}
+const addCartToMemory = () => {
+    localStorage.setItem('cart', JSON.stringify(carts));
+}
+const addCartToHTML = () => {
+    listCartHTML.innerHTML = '';
+    let totalQuantity = 0;
+    if(carts.length > 0){
+        carts.forEach(cart => {
+            totalQuantity = totalQuantity + cart.quantity;
+            let newCart = document.createElement('div');
+            newCart.classList.add('it');
+            newCart.dataset.id = cart.product_id;
+            let positionProduct = listProducts.findIndex((value) => value.id == cart.product_id);
+            let info = listProducts[positionProduct];
+            newCart.innerHTML = `
+            <div class="image">
+          <img src="${info.image}" alt="">
+        </div>
+          <div class="name_cart">
+          ${info.name}
+          </div>
+            <div class="totalPrice">
+          R$${info.price * cart.quantity}
+            </div>
+              <div class="quantity">
+                <span class="menos"><</span>
+                <span>${cart.quantity}</span>
+                <span class="mais">></span>
+            </div>
+            `;
+        listCartHTML.appendChild(newCart);
+        })
+    }
+    iconCartSpan.innerText = totalQuantity;
+}
+//essa
+listCartHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if(positionClick.classList.contains('menos') || positionClick.classList.contains('mais')){
+        let product_id = positionClick.parentElement.parentElement.dataset.id;
+        let type = 'menos';
+        if(positionClick.classList.contains('mais')){
+            type = 'mais';
+        }
+        changeQuantity(product_id, type);
+    }
+})
+const changeQuantity = (product_id, type) => {
+    let positionItemInCart = carts.findIndex((value) => value.product_id == product_id);
+    if(positionItemInCart >= 0){
+        switch (type) {
+            case 'mais':
+                carts[positionItemInCart].quantity = carts[positionItemInCart].quantity + 1;
+                break;
+            
+            default:
+                let valueChange = carts[positionItemInCart].quantity - 1;
+                if(valueChange > 0){
+                    carts[positionItemInCart].quantity = valueChange
+                }else{
+                    carts.splice(positionItemInCart, 1);
+                }
+                break;
+        }
+    }
+    addCartToMemory();
+    addCartToHTML();
+}
+const initApp = () => {
+    // get data from json
+    fetch('products.json')
+    .then(response => response.json())
+    .then(data => {
+        listProducts = data;
+        addDataToHTML();
+
+        // get cart from memory
+        if(localStorage.getItem('cart')){
+            carts = JSON.parse(localStorage.getItem('cart'));
+            addCartToHTML();
+        }
+    })
+}
+initApp();
