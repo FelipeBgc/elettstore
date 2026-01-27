b7780ab5f936fcb78d8ebe7dff201f152658c76a
FelipeBgc <161802322+FelipeBgc@users.noreply.github.com>
2026-01-26 22:38:08 -0300

    paginas da categoria

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,57 @@
-//ANIMAÇÃO SLIDE
+// LISTA 3 PONTINHOS
+
+class MobileNavbar { 
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+        
+        this.handleClick = this.handleClick.bind(this);
+
+        }
+
+        // animação dos links do menu
+        animateLinks() {
+            this.navLinks.forEach((link) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`);
+            });
+        }
+
+        // alternar o menu
+        handleClick() {
+            this.navList.classList.toggle(this.activeClass);
+            this.animateLinks();
+        }
+
+        // evento de clique no menu
+        addClickEvent() {
+            this.mobileMenu.addEventListener("click", this.handleClick);
+        }
+
+        // inicializar o objeto
+        init() {
+            if(this.mobileMenu) {
+                this.addClickEvent();
+            }
+            return this;
+        }
+    }
+    
+    // criando o objeto e iniciando o menu
+    const mobileNavbar = new MobileNavbar(
+        ".mobile-menu",
+        ".nav-list",
+        ".nav-list li",
+    );
+    
+    mobileNavbar.init();
+
+
+
+    //ANIMAÇÃO SLIDE
 
 //SLIDE AUTOMÁTICO
 let count = 1;
@@ -20,7 +73,10 @@ function nextImage(){
 }
 
 
+
 //LISTA NA PESQUISA
+
+
 // obtendo o input de busca
 const searchInput = document.getElementById("search");
 
